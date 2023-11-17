fb0658032cba640006d74a2998cd982fff009daf
Lucas <lucasoliveirag@gmail.com>
2023-11-17 16:59:23 -0300

    Commit Inicial, criação de estruturas de pastas

---
+++ src/Components/Errors/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Errors = () => {
+  return (
+    <View>
+      <Text>Errors</Text>
+    </View>
+  )
+}
+
+export default Errors
---
+++ src/Components/Produtos/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Produtos = () => {
+  return (
+    <View>
+      <Text>Produtos</Text>
+    </View>
+  )
+}
+
+export default Produtos
