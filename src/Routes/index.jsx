caa53df41b822b8fda36d20677b9dc271aa6ca39
Lucas <lucasoliveirag@gmail.com>
2023-11-22 18:13:37 -0300

    criação da pagina sobre

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -3,7 +3,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Home from '../Screens/Home'
 import { Login } from '../Screens/Login'
-import Produto from '../Screens/Produto'
+import Sobre from '../Screens/Sobre'
+
 
 
 const { Navigator, Screen } = createNativeStackNavigator()
@@ -12,6 +13,13 @@ export const Routes = () => {
   return (
     <NavigationContainer>
       <Navigator>
+        <Screen
+          name="Sobre"
+          component={Sobre}
+          options={{
+            // headerShown: false
+          }}
+        />
         <Screen
           name="Login"
           component={Login}
@@ -26,13 +34,13 @@ export const Routes = () => {
             // headerShown: false
           }}
         />
-        <Screen
+        {/* <Screen
           name="Produto"
           component={Produto}
         // options={{
         //   headerShown: false
         // }}
-        />
+        /> */}
       </Navigator>
     </NavigationContainer>
   )
